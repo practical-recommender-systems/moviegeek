dcbcb52ca8f816c25326049c28c8eaddeeae5dd7
Kim Falk <kim.falk@adform.com>
2016-06-01 23:34:36 +0200

    more user view

---
+++ analytics/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+class Rating(models.Model):
+    id = models.AutoField(primary_key=True)
+    user_id = models.IntegerField()
+    movie_id = models.CharField(max_length=8, unique=True, primary_key=True)
+    rating = models.IntegerField()
+    rating_timestamp = models.DateTimeField()
+
+    def __str__(self):
+        return "user_id: {}, content_id: {}, event: {}".format(self.user_id, self.movie_id, self.rating) #B
+
---
+++ analytics/views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render
+
+from moviegeeks.models import Movie
+from analytics.models import Rating
+
+def user(request, user_id):
+
+    user_ratings = Rating.objects.filter(user_id=user_id).order_by('-rating')
+    movies = Movie.objects.filter(movie_id__in=user_ratings.values('movie_id'))
+
+    movie_dtos = list()
+    sum_rating = 0
+    for movie in movies.values():
+        id = movie['movie_id']
+        for rating in user_ratings.values():
+
+            #todo: rating.movieid is an integer
+            if rating['movie_id'] == int(id):
+                r = rating['rating']
+                sum_rating += r
+                movie_dtos.append(Movie_dto(id, movie['title'], r))
+
+    context_dict = {
+        'user_id': user_id,
+        'avg_rating': sum_rating/float(len(movie_dtos)),
+        'movies': movie_dtos,
+
+    }
+    return render(request, 'analytics/user.html', context_dict)
+
+def statistics(request):
+
+    user_by_moviecount = Rating.objects.values('user_id').annotate(movie_count=count('movie_id')).order_by('-movie_count')
+
+
+class Movie_dto(object):
+
+    def __init__(self, movie_id, title, rating):
+        self.movie_id = movie_id
+        self.title = title
+        self.rating = rating
+
