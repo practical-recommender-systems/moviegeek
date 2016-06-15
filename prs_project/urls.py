6a03a8385293fef28cdce38c18f057b9f58eda6e
Kim Falk <kim.falk@adform.com>
2016-06-15 17:44:07 +0200

    Adding recommender app

--- prs_project/urls.py
+++ prs_project/urls.py
@@ -1,18 +1,4 @@
-"""prs_project URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.9/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
-"""
+"""prs_project URL Configuration"""
 from django.conf.urls import url, include
 from django.contrib import admin
 from moviegeeks import views
@@ -23,4 +9,5 @@ urlpatterns = [
     url(r'^collect/', include('collector.urls')),
     url(r'^analytics/', include('analytics.urls')),
     url(r'^admin/', admin.site.urls),
+    url(r'^rec/', include('recommender.urls'))
 ]
