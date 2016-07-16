312b3dca06eed1f1797ba98e486c4bfb6b91b7c3
Kim Falk <kim.falk@adform.com>
2016-07-16 17:02:29 +0200

    Script updated
    - outputs to a csv file now

--- populate_sample_of_descriptions.py
+++ populate_sample_of_descriptions.py
@@ -29,6 +29,30 @@ def get_descriptions():
 
         print("{}: {}".format(page, r.json()))
 
+def save_as_csv():
+    url = """https://api.themoviedb.org/3/discover/movie?primary_release_date.gte=2016-01-01&primary_release_date.lte=2016-10-22&api_key={}&page={}"""
+    api_key = get_api_key()
+
+    file = open('data.json','w')
+
+    films = []
+    for page in range(1, 50):
+        r = requests.get(url.format(api_key, page))
+        for film in r.json()['results']:
+            f = dict()
+
+            f['id'] = film['id']
+            f['imdb_id'] = get_imdb_id(f['id'])
+            f['title'] = film['title']
+            f['description'] = film['overview']
+            f['genres'] = film['genre_ids']
+            films.append(f)
+        print("{}: {}".format(page, r.json()))
+
+    json.dump(films, file, sort_keys=True, indent=4)
+
+    file.close()
+
 
 def get_imdb_id(moviedb_id):
     url = """https://api.themoviedb.org/3/movie/{}?api_key={}"""
@@ -47,7 +71,6 @@ def get_popular_films_for_genre(genre_str):
     film_genres = {'drama': 18, 'action': 28, 'comedy': 35}
     genre = film_genres[genre_str]
 
-    action = 28
     url = """https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&with_genres={}&api_key={}"""
     api_key = get_api_key()
     r = requests.get(url.format(genre, api_key))
@@ -63,5 +86,6 @@ def get_popular_films_for_genre(genre_str):
 
 if __name__ == '__main__':
     print("Starting MovieGeeks Population script...")
-    # get_descriptions()
-    get_popular_films_for_genre('comedy')
+    #get_descriptions()
+    # get_popular_films_for_genre('comedy')
+    save_as_csv()
