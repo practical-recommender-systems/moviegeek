9952d269dabb07262e048db1baa80c99b07fa6a0
Kim FalkJørgensen <kim.falk@karnovgroup.com>
2017-05-30 10:01:49 +0200

    * Updated the similarity method, to use a sparse matrix. * Mask similarites better * Added a small test data in a main

--- builder/item_similarity_calculator.py
+++ builder/item_similarity_calculator.py
@@ -9,11 +9,14 @@ django.setup()
 import datetime
 import decimal
 import pandas as pd
+import math
 import numpy as np
 import time
 
 from recommender.models import Similarity
 from analytics.models import Rating
+from sklearn.metrics.pairwise import cosine_similarity
+from scipy import sparse
 
 
 class ItemSimilarityMatrixBuilder(object):
@@ -21,52 +24,54 @@ class ItemSimilarityMatrixBuilder(object):
     def __init__(self, min_overlap=5):
         self.min_overlap = min_overlap
 
-    def save_cf(self, df, created=datetime.datetime.now()):
-        print("Save item-item model")
+    def save_sparse_matrix(self, sm, index, created=datetime.datetime.now()):
 
+        print('saving similarities (number:{})'.format(sm.shape[0]*sm.shape[1]))
         Similarity.objects.all().delete()
         sims = []
-        inx = 0
-        for row in df.iterrows():
-            if inx == 100:
+
+        rows, cols = self.interesting_indexes(sm, 0.2)
+        for row, col in zip(rows, cols):
+
+            if len(sims) == 1000:
                 Similarity.objects.bulk_create(sims)
                 sims = []
 
-            else:
-                inx += 1
+            if row != col:
+                new_similarity = Similarity(
+                    created=created,
+                    source=index[row],
+                    target=index[col],
+                    similarity=decimal.Decimal(str(sm[row, col]))
+                )
 
-            new_similarity = Similarity(
-                created=created,
-                source=int(row[0]),
-                target=int(row[1].values[0]),
-                similarity=decimal.Decimal(str(row[1].values[1]))
-            )
-            if not new_similarity.target == new_similarity.source and new_similarity.similarity > 0:
                 sims.append(new_similarity)
+
         Similarity.objects.bulk_create(sims)
         print('Similarity items saved')
 
+    def interesting_indexes(self, sm, min_sim):
+
+        cors = sm.tocoo()
+        nz_mask = (not np.all(np.isnan(cors.data))) and cors.data > min_sim
+        return cors.row[nz_mask], cors.col[nz_mask]
+
     def build(self, ratings):
-        print("calculating the similarities.")
-        print("Calculating similarites ... using {} ratings".format(len(ratings)))
+        print("Calculating similarities ... using {} ratings".format(len(ratings)))
 
         ratings['avg'] = ratings.groupby('user_id')['rating'].transform(lambda x: normalize(x))
         ratings['avg'] = ratings['avg'].astype(float)
-        print("normalized ratings.")
-        print(ratings.head())
 
-        rp = ratings.pivot_table(index=['movie_id'], columns=['user_id'], values='avg')
+        print("normalized ratings.")
+
+        rp = ratings.pivot_table(index=['movie_id'], columns=['user_id'], values='avg', fill_value=0)
         print("rating matrix finished")
 
-        cor = rp.transpose().corr(method='pearson', min_periods=self.min_overlap)
-
-        # todo: implement cosine_similarity
-        # cor = cosine_similarity(rp.trainspose())
+        #cor = cosine_similarity(rp, dense_output=False)
+        cor = sparse.csr_matrix(rp.transpose().corr(method='pearson', min_periods=20))
         print('correlation is finished')
 
-        long_format_cor = cor.stack().reset_index(level=0)
-
-        self.save_cf(long_format_cor)
+        self.save_sparse_matrix(cor, rp.index)
 
         return cor
 
@@ -75,7 +80,7 @@ def normalize(x):
     x = x.astype(float)
     if x.std() == 0:
         return 0.0
-    return (x - x.mean()) / x.std()
+    return (x - x.mean()) / (x.max() - x.min())
 
 
 def split_ratings2(min_rank=3):
@@ -99,6 +104,35 @@ def load_all_ratings():
 
 
 if __name__ == '__main__':
-    print("Load ratings...")
+    TEST = True
 
-    ItemSimilarityMatrixBuilder().build(load_all_ratings())
+    if TEST:
+        ratings = pd.DataFrame(
+            [[1, '0011', 5, '2013-10-12 23:20:27+00:00'],
+             [1, '12', 3, '2014-10-12 23:20:27+00:00'],
+             [1, '14', 2, '2015-10-12 23:20:27+00:00'],
+             [2, '0011', 4, '2013-10-12 23:20:27+00:00'],
+             [2, '12', 3, '2014-10-12 23:20:27+00:00'],
+             [2, '13', 4, '2015-10-12 23:20:27+00:00'],
+             [3, '0011', 5, '2013-10-12 23:20:27+00:00'],
+             [3, '12', 2, '2014-10-12 23:20:27+00:00'],
+             [3, '13', 5, '2015-10-12 23:20:27+00:00'],
+             [3, '14', 2, '2016-10-12 23:20:27+00:00'],
+             [4, '0011', 3, '2013-10-12 23:20:27+00:00'],
+             [4, '12', 5, '2014-10-12 23:20:27+00:00'],
+             [4, '13', 3, '2015-10-12 23:20:27+00:00'],
+             [5, '0011', 3, '2013-10-12 23:20:27+00:00'],
+             [5, '12', 3, '2014-10-12 23:20:27+00:00'],
+             [5, '13', 3, '2015-10-12 23:20:27+00:00'],
+             [5, '14', 2, '2016-10-12 23:20:27+00:00'],
+             [6, '0011', 2, '2013-10-12 23:20:27+00:00'],
+             [6, '12', 3, '2014-10-12 23:20:27+00:00'],
+             [6, '13', 2, '2015-10-12 23:20:27+00:00'],
+             [6, '14', 3, '2016-10-12 23:20:27+00:00'],
+             ], columns=['user_id', 'movie_id', 'rating', 'rating_timestamp'])
+
+        result = ItemSimilarityMatrixBuilder().build(ratings)
+        print(result)
+
+    else:
+        ItemSimilarityMatrixBuilder().build(load_all_ratings())
