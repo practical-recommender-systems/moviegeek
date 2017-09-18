a4f6514e8ca120a259a2710f99cb413ce2e35fbc
Kim FalkJørgensen <kim.falk@karnovgroup.com>
2017-09-18 21:37:18 +0200

    * its working, still needs to be cleaned up

--- builder/evaluation_runner.py
+++ builder/evaluation_runner.py
@@ -2,20 +2,25 @@ from decimal import Decimal
 import os
 
 import datetime
+import math
 import pandas as pd
 import numpy as np
+import timeit
 
 from sklearn.model_selection import KFold
 
-from builder.algorithm_evaluator import PrecissionAtK, MeanAverageError
+from builder.algorithm_evaluator import PrecisionAtK, MeanAverageError
 from builder.item_similarity_calculator import ItemSimilarityMatrixBuilder
 from recs.neighborhood_based_recommender import NeighborhoodBasedRecs
 from django.db.models import Count
+
+from recs.popularity_recommender import PopularityBasedRecs
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "prs_project.settings")
 import django
 from datetime import datetime
 import time
+from sklearn.model_selection import train_test_split
 
 django.setup()
 
@@ -31,7 +36,7 @@ class EvaluationRunner(object):
         self.K = K
 
     def clean_data(self, ratings, min_ratings=5):
-        print("cleaning data only to contain users with atleast {} ratings".format(min_ratings))
+        print("cleaning data only to contain users with at least {} ratings".format(min_ratings))
 
         original_size = ratings.shape[0]
 
@@ -53,14 +58,52 @@ class EvaluationRunner(object):
             ratings_rows = Rating.objects.all().values()
 
         else:
-            user_ids = Rating.objects.values('user_id').annotate(movie_count=Count('movie_id')).order_by('-movie_count')
+            user_ids = Rating.objects.values('user_id')\
+                .annotate(movie_count=Count('movie_id'))\
+                .filter(movie_count__gt=min_number_of_ratings)\
+                .order_by('-movie_count')
+
             user_ids = user_ids.values('user_id')[:number_test_users]
 
             ratings_rows = Rating.objects.filter(user_id__in=user_ids).values()
 
         all_ratings = pd.DataFrame.from_records(ratings_rows)
+        if self.folds == 0:
+            return self.calculate_using_ratings_no_crossvalidation(all_ratings,
+                                                min_number_of_ratings,
+                                                min_rank)
+        else:
+            return self.calculate_using_ratings(all_ratings,
+                                                min_number_of_ratings,
+                                                min_rank)
 
-        return self.calculate_using_ratings(all_ratings, min_number_of_ratings, min_rank)
+    def calculate_using_ratings_no_crossvalidation(self, all_ratings, min_number_of_ratings=5, min_rank=5):
+        ratings = self.clean_data(all_ratings, min_number_of_ratings)
+
+        users = ratings.user_id.unique()
+
+        train_data_len = int((len(users)*70/100))
+        np.random.shuffle(users)
+        train_users, test_users = users[:train_data_len], users[train_data_len:]
+
+        test_data, train_data = self.split_data(min_rank,
+                                                ratings,
+                                                test_users,
+                                                train_users)
+
+        print("Test run having {} training rows, and {} test rows".format(len(train_data),
+                                                                          len(test_data)))
+
+        if self.builder:
+            self.builder.build(train_data)
+
+        print("Build is finished")
+
+        pak, rak = PrecisionAtK(self.K, self.recommender).calculate(train_data, test_data)
+        mae=0
+        #mae = MeanAverageError(self.recommender).calculate(train_data, test_data)
+        results = {'pak': pak, 'rak': rak, 'mae': mae}
+        return results
 
     def calculate_using_ratings(self, all_ratings, min_number_of_ratings=5, min_rank=5):
 
@@ -71,6 +114,7 @@ class EvaluationRunner(object):
 
         validation_no = 0
         paks = Decimal(0.0)
+        raks = Decimal(0.0)
         maes = Decimal(0.0)
 
         for train, test in kf.split(users):
@@ -82,14 +126,19 @@ class EvaluationRunner(object):
                                                     users[test],
                                                     users[train])
 
-            print("Test run having {} training rows, and {} test rows".format(len(train_data), len(test_data)))
-            self.builder.build(train_data)
+            print("Test run having {} training rows, and {} test rows".format(len(train_data),
+                                                                              len(test_data)))
+            if self.builder:
+                self.builder.build(train_data)
+
             print("Build is finished")
 
-            paks += PrecissionAtK(self.K, self.recommender).calculate(train_data, test_data)
+            pak, rak = PrecisionAtK(self.K, self.recommender).calculate(train_data, test_data)
+
+            paks += pak
+            raks += rak
             maes += MeanAverageError(self.recommender).calculate(train_data, test_data)
-            print("MAE = {}".format(maes))
-            results = {'pak': paks/self.folds, 'mae': maes/self.folds}
+            results = {'pak': paks/self.folds, 'rak': raks/self.folds, 'mae': maes/self.folds}
 
         print(results)
         return results
@@ -99,43 +148,52 @@ class EvaluationRunner(object):
         return kf
 
     def split_data(self, min_rank, ratings, test_users, train_users):
-        train = ratings[ratings['user_id'].isin(train_users)]
-        test_temp = ratings[ratings['user_id'].isin(test_users)]
 
-        test_temp['rank'] = test_temp.groupby('user_id')['rating_timestamp'].rank(ascending=False)
-        test = test_temp[test_temp['rank'] > min_rank]
-        additional_training_data = test_temp[test_temp['rank'] >= min_rank]
+        train = ratings[ratings['user_id'].isin(train_users)]
+
+        test_temp = ratings[ratings['user_id'].isin(test_users)].sort_values('rating_timestamp', ascending=False)
+
+        test = test_temp.groupby('user_id').head(min_rank)
+
+        additional_training_data = test_temp[~test_temp.index.isin(test.index)]
 
         train = train.append(additional_training_data)
 
         return test, train
 
 if __name__ == '__main__':
-    min_number_of_ratings = 30
-    min_overlap = 25
-    min_sim = 0
-    K = 25 #redo
+    min_number_of_ratings = 20
+    min_overlap = 5
+    min_sim = 0.2
+    K = 20
     min_rank = 5
 
     timestr = time.strftime("%Y%m%d-%H%M%S")
-    file_name = '{}-min_number_of_ratings_training.csv'.format(timestr)
+    file_name = '{}-min_number_of_ratings_training_item_similarity.csv'.format(timestr)
 
     with open(file_name, 'a', 1) as logfile:
-        logfile.write("pak, mae, min_overlap, min_sim, K, min_num_of_ratings, min_rank\n")
+        logfile.write("rak, pak, mae, min_overlap, min_sim, K, min_num_of_ratings, min_rank\n")
 
-        for min_number_of_ratings in np.arange(5, 30, 10):
+        builder = ItemSimilarityMatrixBuilder(min_overlap, min_sim=min_sim)
+
+        for min_number_of_ratings in np.arange(10, 150, 10):
             min_rank = min_number_of_ratings/2
-            min_overlap = min_number_of_ratings - min_rank
-            er = EvaluationRunner(3,
-                                  ItemSimilarityMatrixBuilder(min_overlap, min_sim=min_sim),
-                                  NeighborhoodBasedRecs(),
-                                  K)
-            result = er.calculate(min_number_of_ratings, min_rank, number_test_users=1000)
+            er = EvaluationRunner(0,
+                                   builder,
+                                   NeighborhoodBasedRecs(),
+                                   K)
+            # Run the baseline recommender:
+            # er = EvaluationRunner(3, None, PopularityBasedRecs(), K)
+
+            result = er.calculate(min_number_of_ratings, min_rank, number_test_users=-1)
             pak = result['pak']
             mae = result['mae']
-            logfile.write("{}, {}, {}, {}, {}, {}, {} \n".format(pak, mae, min_overlap,
+            rak = result['rak']
+            logfile.write("{}, {}, {}, {}, {}, {}, {}, {} \n".format(rak, pak, mae, min_overlap,
                                                                  min_sim, K,
                                                                  min_number_of_ratings,
                                                                  min_rank, datetime.now()))
+            logfile.flush()
+            #builder = None
 
 
