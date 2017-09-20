1648b5b84d5bfda32ee84f4b1ef459d1ed472704
Kim FalkJørgensen <kim.falk@karnovgroup.com>
2017-09-20 11:30:13 +0200

    * refactoring the item-item neighbourhood recommender (more to come)

--- recs/neighborhood_based_recommender.py
+++ recs/neighborhood_based_recommender.py
@@ -2,6 +2,7 @@ from recs.base_recommender import base_recommender
 from analytics.models import Rating
 from recommender.models import Similarity
 from django.db.models import Q
+import time
 
 from decimal import Decimal
 
@@ -16,33 +17,36 @@ class NeighborhoodBasedRecs(base_recommender):
 
     def recommend_items_by_ratings(self, user_id, active_user_items, num=6):
 
+        start = time.time()
         movie_ids = {movie['movie_id']: movie['rating'] for movie in active_user_items}
-        user_mean = sum(movie_ids.values()) / Decimal(len(movie_ids))
+        user_mean = Decimal(sum(movie_ids.values())) / Decimal(len(movie_ids))
 
-        candidate_items = Similarity.objects.filter(source__in=movie_ids.keys())
-        candidate_items = candidate_items.distinct().order_by('-similarity')[:50]
+        candidate_items = Similarity.objects.filter(Q(source__in=movie_ids.keys())
+                                                    & ~Q(target__in=movie_ids.keys()))
+        candidate_items = list(candidate_items.distinct().order_by('-similarity')[:20])
+        #print("Time spend loading candidates", user_id, " time: ", time.time() - start)
+        #start = time.time()
 
         recs = dict()
         for candidate in candidate_items:
             target = candidate.target
 
-            if target not in movie_ids:
-                pre = 0
-                sim_sum = 0
+            pre = 0
+            sim_sum = 0
 
-                rated_items = [i for i in candidate_items if i.target == target][:self.neighborhood_size]
+            rated_items = [i for i in candidate_items if i.target == target][:self.neighborhood_size]
 
-                if len(rated_items) > 0:
-                    for sim_item in rated_items:
-                        r = movie_ids[sim_item.source] - user_mean
-                        pre += sim_item.similarity * r
-                        sim_sum += sim_item.similarity
-                    if sim_sum > 0:
-                        recs[target] = {'prediction': user_mean + pre / sim_sum,
-                                        'sim_items': [r.source for r in rated_items]}
+            if len(rated_items) > 0:
+                for sim_item in rated_items:
+                    r = Decimal(movie_ids[sim_item.source]) - user_mean
+                    pre += sim_item.similarity * r
+                    sim_sum += sim_item.similarity
+                if sim_sum > 0:
+                    recs[target] = {'prediction': user_mean + pre / sim_sum,
+                                    'sim_items': [r.source for r in rated_items]}
 
         sorted_items = sorted(recs.items(), key=lambda item: -float(item[1]['prediction']))[:num]
-
+        print("Time spend on rec ", user_id, " time: ", time.time() - start)
         return sorted_items
 
     def predict_score(self, user_id, item_id):
@@ -57,7 +61,7 @@ class NeighborhoodBasedRecs(base_recommender):
         bottom = 0
 
         candidate_items = Similarity.objects.filter(source__in=movie_ids.keys()).filter(target=item_id)
-        candidate_items = candidate_items.distinct().order_by('-similarity')
+        candidate_items = candidate_items.distinct().order_by('-similarity')[:100]
 
         if len(candidate_items) == 0:
             return 0
