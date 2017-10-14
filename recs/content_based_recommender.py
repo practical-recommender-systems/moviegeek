5b7bcb9566801e543b593c2a915e093e9cbfc8c1
Kim FalkJørgensen <kim.falk@karnovgroup.com>
2017-10-14 18:00:53 +0200

    * using the similarities from the database instead.

--- recs/content_based_recommender.py
+++ recs/content_based_recommender.py
@@ -4,59 +4,91 @@ from datetime import datetime
 
 from recs.base_recommender import base_recommender
 from analytics.models import Rating
-from recommender.models import MovieDescriptions
-
+from recommender.models import MovieDescriptions, LdaSimilarity
+from django.db.models import Q
 from gensim import models, corpora, similarities, matutils
+from decimal import Decimal
 
 lda_path = './lda/'
 
 
 class ContentBasedRecs(base_recommender):
 
-    def __init__(self,
-                 corpus=corpora.MmCorpus(lda_path + 'corpus.mm'),
-                 index=similarities.MatrixSimilarity.load(lda_path + 'index.lda')):
-        self.corpus = corpus
-        self.index = index
+    def __init__(self, min_sim=0.1):
+        self.min_sim = min_sim
+        self.max_candidates = 100
 
-    def recommend_items(self,
-                        user_id,
-                        num=6):
+    def recommend_items(self, user_id, num=6):
 
-        movie_ids = Rating.objects.filter(user_id=user_id).order_by('-rating').values_list('movie_id', flat=True)[:100]
+        active_user_items = Rating.objects.filter(user_id=user_id).order_by('-rating')[:100]
 
-        return self.recommend_items_from_items(movie_ids, num)
+        return self.recommend_items_by_ratings(user_id, active_user_items.values(), num)
 
-    def recommend_items_by_ratings(self, user_id, active_user_items, num=6):
-        movie_ids = [movie['movie_id'] for movie in active_user_items]
-        return self.recommend_items_from_items(movie_ids, num)
+    def seeded_rec(self, content_ids, take=6):
+        data = LdaSimilarity.objects.filter(source__in=content_ids) \
+                   .order_by('-similarity') \
+                   .values('target', 'similarity')[:take]
+        return list(data)
 
-    def recommend_items_from_items(self,
-                                   movie_ids,
+    def recommend_items_by_ratings(self,
+                                   user_id,
+                                   active_user_items,
                                    num=6):
         content_sims = dict()
 
         start_time = datetime.now()
-        for movie_id in movie_ids:
 
-            md = MovieDescriptions.objects.filter(imdb_id=movie_id).first()
-            if md is not None:
+        movie_ids = {movie['movie_id']: movie['rating'] for movie in active_user_items}
+        user_mean = sum(movie_ids.values()) / len(movie_ids)
 
-                lda_vector = self.corpus[int(md.lda_vector)]
-                sims = self.index[lda_vector]
-                sorted_sims = sorted(enumerate(sims), key=lambda item: -item[1])[1:num+5]
-                movies = get_movie_ids(sorted_sims)
+        sims = LdaSimilarity.objects.filter(Q(source__in=movie_ids.keys())
+                                            & ~Q(target__in=movie_ids.keys())
+                                            & Q(similarity__gt=self.min_sim))
+        sims = sims.order_by('-similarity')[:self.max_candidates]
+        recs = dict()
 
-                for movie in movies:
-                    target = movie['target']
-                    if target in content_sims.keys():
-                        if movie['sim'] > content_sims[target]['sim']:
-                            content_sims[target] = movie
-                    else:
-                        content_sims[target] = movie
-        print(f"old way {datetime.now()-start_time}")
+        for sim in sims:
+            target = sim.target
 
-        return sorted(content_sims.items(), key=lambda item: -float(item[1]['sim']))[:num]
+            pre = 0
+            sim_sum = 0
+
+            rated_items = [i for i in sims if i.target == target]
+
+            if len(rated_items) > 0:
+                for sim_item in rated_items:
+                    r = Decimal(movie_ids[sim_item.source] - user_mean)
+                    pre += sim_item.similarity * r
+                    sim_sum += sim_item.similarity
+
+                if sim_sum > 0:
+                    recs[target] = { 'prediction': Decimal(user_mean) + pre/sim_sum,
+                                     'sim_items': [r.source for r in rated_items]}
+        print(f"new way {datetime.now()-start_time}")
+
+        return sorted(recs.items(), key=lambda item: -float(item[1]['prediction']))[:num]
+
+        # print(list(sims))
+        # for movie_id in movie_ids:
+        #
+        #     md = MovieDescriptions.objects.filter(imdb_id=movie_id).first()
+        #     if md is not None:
+        #
+        #         lda_vector = self.corpus[int(md.lda_vector)]
+        #         sims = self.index[lda_vector]
+        #         sorted_sims = sorted(enumerate(sims), key=lambda item: -item[1])[1:num+5]
+        #         movies = get_movie_ids(sorted_sims)
+        #
+        #         for movie in movies:
+        #             target = movie['target']
+        #             if target in content_sims.keys():
+        #                 if movie['sim'] > content_sims[target]['sim']:
+        #                     content_sims[target] = movie
+        #             else:
+        #                 content_sims[target] = movie
+        # print(f"old way {datetime.now()-start_time}")
+        #
+        # return sorted(content_sims.items(), key=lambda item: -float(item[1]['sim']))[:num]
 
     def predict_score(self, user_id, item_id):
 
