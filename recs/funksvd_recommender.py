be6e19087b34293e3e4b19801cf645c092a826cd
Kim FalkJørgensen <kim.falk@karnovgroup.com>
2017-10-28 10:41:38 +0200

    * funkSVD updated to run faster • added tests

--- recs/funksvd_recommender.py
+++ recs/funksvd_recommender.py
@@ -1,10 +1,38 @@
+import json
+
+from django.db.models import Avg
+
 from recommender.models import Recs
+from analytics.models import Rating
 from recs.base_recommender import base_recommender
+
 import numpy as np
 import pandas as pd
 import pickle
 
 class FunkSVDRecs(base_recommender):
+
+
+    def __init__(self, save_path='/Users/u0157492/Projects/moviegeek/models/hyperparameter/model/40factors/'):
+        self.save_path = save_path
+        self.load_model(save_path)
+
+        self.avg = list(Rating.objects.all().aggregate(Avg('rating')).values())[0]
+
+    def load_model(self, save_path):
+        with open(save_path + 'user_bias.data', 'rb') as ub_file:
+            self.user_bias = pickle.load(ub_file)
+        with open(save_path + 'item_bias.data', 'rb') as ub_file:
+            self.item_bias = pickle.load(ub_file)
+        with open(save_path + 'user_factors.json', 'r') as infile:
+            self.user_factors = pd.DataFrame(json.load(infile)).T
+        with open(save_path + 'item_factors.json', 'r') as infile:
+            self.item_factors = pd.DataFrame(json.load(infile)).T
+
+    def set_save_path(self, save_path):
+        self.save_path = save_path
+
+        self.load_model(save_path)
 
     def predict_score(self, user_id, item_id):
         rec = Recs.objects.filter(user_id = user_id, item_id=item_id).first()
@@ -14,8 +42,31 @@ class FunkSVDRecs(base_recommender):
             return rec.rating
 
     def recommend_items(self, user_id, num=6):
-        recs = Recs.objects.filter(user_id=user_id, item_id=item_id).orderby('rating')[:6]
-        if recs is None:
-            return []
-        else:
-            return recs.values()
+        active_user_items = Rating.objects.filter(user_id=user_id).order_by('-rating')[:100]
+
+        return self.recommend_items_by_ratings(user_id, active_user_items.values())
+
+    def recommend_items_by_ratings(self, user_id, active_user_items, num=6):
+
+        rated_movies = {movie['movie_id']: movie['rating'] for movie in active_user_items}
+
+        user = self.user_factors[str(user_id)]
+        scores = self.item_factors.T.dot(user)
+
+        scores.sort_values(inplace=True, ascending=False)
+        result = scores[:num + len(rated_movies)]
+        user_bias = 0
+
+        if user_id in self.user_bias.keys():
+            user_bias = self.user_bias[user_id]
+        elif int(user_id) in self.user_bias.keys():
+            user_bias = self.user_bias[int(user_id)]
+
+        recs = {r[0]: {'prediction': r[1] + user_bias + self.item_bias[r[0]] + self.avg}
+                for r in zip(result.index, result) if r[0] not in rated_movies}
+
+        sorted_items = sorted(recs.items(), key=lambda item: -float(item[1]['prediction']))[:num]
+        if user_id == 14620:
+            print(user_id, rated_movies)
+            print(user_id, sorted_items)
+        return sorted_items
---
+++ test/funksvd_recommender_test.py
@@ -0,0 +1,192 @@
+import os
+
+from builder.matrix_factorization_calculator import MatrixFactorization
+from recs.funksvd_recommender import FunkSVDRecs
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "prs_project.settings")
+
+import django
+
+django.setup()
+
+import unittest
+
+import pandas as pd
+
+from recs.neighborhood_based_recommender import NeighborhoodBasedRecs
+from recommender.models import Similarity
+from builder.item_similarity_calculator import ItemSimilarityMatrixBuilder
+
+STAR_WARS = 'star wars'
+WONDER_WOMAN = 'wonder woman'
+AVENGERS = 'avengers'
+WOLVERINE = 'logan'
+PIRATES_OF = 'pirates of the caribbien'
+HARRY = 'harry potter I'
+CAPTAIN_AMERICA = 'captain america'
+ALIEN = 'alien'
+DR_STRANGELOVE = 'doctor strangelove'
+JACQUES = 'jacques'
+
+
+class TestNeighborhoodBasedRecs(unittest.TestCase):
+    def setUp(self):
+        self.ratings = pd.DataFrame(
+            [['1', STAR_WARS, 9, '2013-10-12 23:21:27+00:00'],
+             ['1', WONDER_WOMAN, 10, '2014-10-12 23:22:27+00:00'],
+             ['1', AVENGERS, 10, '2015-11-12 23:20:27+00:00'],
+             ['1', WOLVERINE, 8, '2015-08-12 23:20:27+00:00'],
+             ['1', PIRATES_OF, 10, '2015-10-12 22:20:27+00:00'],
+             ['1', HARRY, 10, '2015-10-12 23:21:27+00:00'],
+             ['1', CAPTAIN_AMERICA, 10, '2014-10-12 23:20:27+00:00'],
+             ['1', ALIEN, 6, '2015-10-12 23:22:27+00:00'],
+             ['1', JACQUES, 6, '2015-10-12 11:20:27+00:00'],
+
+             ['2', STAR_WARS, 10, '2013-10-12 23:20:27+00:00'],
+             ['2', WONDER_WOMAN, 10, '2014-10-12 23:20:27+00:00'],
+             ['2', AVENGERS, 9, '2016-10-12 23:20:27+00:00'],
+             ['2', PIRATES_OF, 6, '2010-10-12 23:20:27+00:00'],
+             ['2', CAPTAIN_AMERICA, 10, '2005-10-12 23:20:27+00:00'],
+             ['2', DR_STRANGELOVE, 10, '2015-01-12 23:20:27+00:00'],
+
+             ['3', STAR_WARS, 9, '2013-10-12 20:20:27+00:00'],
+             ['3', AVENGERS, 10, '2015-10-12 10:20:27+00:00'],
+             ['3', PIRATES_OF, 9, '2013-03-12 23:20:27+00:00'],
+             ['3', HARRY, 8, '2016-10-13 23:20:27+00:00'],
+             ['3', DR_STRANGELOVE, 10, '2016-09-12 23:20:27+00:00'],
+
+             ['4', STAR_WARS, 8, '2013-10-12 23:20:27+00:00'],
+             ['4', WONDER_WOMAN, 8, '2014-10-12 23:20:27+00:00'],
+             ['4', AVENGERS, 9, '2015-10-12 23:20:27+00:00'],
+             ['4', PIRATES_OF, 5, '2013-10-12 23:20:27+00:00'],
+             ['4', HARRY, 6, '2014-10-12 23:20:27+00:00'],
+             ['4', ALIEN, 8, '2015-10-12 23:20:27+00:00'],
+             ['4', DR_STRANGELOVE, 9, '2015-10-12 23:20:27+00:00'],
+
+             ['5', STAR_WARS, 6, '2013-10-12 23:20:27+00:00'],
+             ['5', AVENGERS, 1, '2014-10-12 23:20:27+00:00'],
+             ['5', WOLVERINE, 2, '2015-10-12 23:20:27+00:00'],
+             ['5', PIRATES_OF, 2, '2016-10-12 23:20:27+00:00'],
+             ['5', HARRY, 10, '2016-10-12 23:20:27+00:00'],
+             ['5', CAPTAIN_AMERICA, 1, '2016-10-12 23:20:27+00:00'],
+             ['5', ALIEN, 4, '2016-10-12 23:20:27+00:00'],
+             ['5', DR_STRANGELOVE, 3, '2016-10-12 23:20:27+00:00'],
+             ['5', JACQUES, 10, '2016-10-12 23:20:27+00:00'],
+
+             ['6', STAR_WARS, 8, '2013-10-12 23:20:27+00:00'],
+             ['6', WONDER_WOMAN, 8, '2014-10-12 23:20:27+00:00'],
+             ['6', AVENGERS, 8, '2014-10-12 23:20:27+00:00'],
+             ['6', WOLVERINE, 8, '2015-10-12 23:20:27+00:00'],
+             ['6', PIRATES_OF, 6, '2016-10-12 23:20:27+00:00'],
+             ['6', HARRY, 10, '2016-10-12 23:20:27+00:00'],
+             ['6', JACQUES, 8, '2016-10-12 23:20:27+00:00'],
+
+             ['7', AVENGERS, 10, '2014-10-12 23:20:27+00:00'],
+             ['7', PIRATES_OF, 3, '2016-10-12 23:20:27+00:00'],
+             ['7', HARRY, 1, '2016-10-12 23:20:27+00:00'],
+             ['7', ALIEN, 8, '2016-10-12 23:20:27+00:00'],
+             ['7', DR_STRANGELOVE, 10, '2016-10-12 23:20:27+00:00'],
+
+             ['8', STAR_WARS, 9, '2013-10-12 23:20:27+00:00'],
+             ['8', WONDER_WOMAN, 7, '2014-10-12 23:20:27+00:00'],
+             ['8', AVENGERS, 7, '2014-10-12 23:20:27+00:00'],
+             ['8', WOLVERINE, 7, '2015-10-12 23:20:27+00:00'],
+             ['8', PIRATES_OF, 8, '2016-10-12 23:20:27+00:00'],
+             ['8', HARRY, 8, '2016-10-12 23:20:27+00:00'],
+             ['8', ALIEN, 8, '2016-10-12 23:20:27+00:00'],
+             ['8', DR_STRANGELOVE, 8, '2016-10-12 23:20:27+00:00'],
+             ['8', JACQUES, 10, '2016-10-12 23:20:27+00:00'],
+
+             ['9', WONDER_WOMAN, 7, '2014-10-12 23:20:27+00:00'],
+             ['9', AVENGERS, 8, '2014-10-12 23:20:27+00:00'],
+             ['9', WOLVERINE, 8, '2015-10-12 23:20:27+00:00'],
+             ['9', PIRATES_OF, 7, '2016-10-12 23:20:27+00:00'],
+             ['9', HARRY, 8, '2016-10-12 23:20:27+00:00'],
+             ['9', CAPTAIN_AMERICA, 10, '2016-10-12 23:20:27+00:00'],
+             ['9', DR_STRANGELOVE, 10, '2016-10-12 23:20:27+00:00'],
+             ['9', JACQUES, 7, '2016-10-12 23:20:27+00:00'],
+
+             ['10', AVENGERS, 7, '2014-10-12 23:20:27+00:00'],
+             ['10', ALIEN, 10, '2016-10-12 23:20:27+00:00'],
+             ['10', CAPTAIN_AMERICA, 6, '2016-10-12 23:20:27+00:00'],
+             ['10', DR_STRANGELOVE, 8, '2016-10-12 23:20:27+00:00'],
+
+             ], columns=['user_id', 'movie_id', 'rating', 'rating_timestamp'])
+
+        self.save_path = './test/'
+        self.k=3
+        MF = MatrixFactorization(save_path=self.save_path)
+        MF.train(self.ratings, k=self.k)
+
+    def test_rec(self):
+
+        recommender = FunkSVDRecs(self.save_path)
+        recs = recommender.recommend_items_by_ratings('1',
+                                                      [{'movie_id': AVENGERS, 'rating': 7},
+                                                       {'movie_id': ALIEN, 'rating': 10},
+                                                       {'movie_id': CAPTAIN_AMERICA, 'rating': 6}], num=2)
+        self.assertIsNotNone(recs)
+        self.assertEqual(len(recs), 2)
+
+
+    def test_rec2(self):
+        recommender = FunkSVDRecs(self.save_path)
+        recs = recommender.recommend_items_by_ratings('5',
+                                                      [{'movie_id': AVENGERS, 'rating': 1}], num=5)
+        self.assertIsNotNone(recs)
+        self.assertEqual(len(recs), 5)
+        top = [r[0] for r in recs][:2]
+        self.assertIn(HARRY, top, '{} was missing from {}'.format(HARRY, top))
+        self.assertIn(JACQUES, top, '{} was missing from {}'.format(JACQUES, top))
+
+
+    def test_rec_increasing(self):
+        recommender = FunkSVDRecs(self.save_path)
+        recs1 = recommender.recommend_items_by_ratings('5',
+                                                      [{'movie_id': AVENGERS, 'rating': 1}], num=2)
+        self.assertIsNotNone(recs1)
+        self.assertEqual(len(recs1), 2)
+
+        recs2 = recommender.recommend_items_by_ratings('5',
+                                                       [{'movie_id': AVENGERS, 'rating': 1}], num=3)
+        self.assertIsNotNone(recs2)
+        self.assertEqual(len(recs2), 3)
+
+        self.assertEqual(recs1[0],recs2[0] )
+        self.assertEqual(recs1[1],recs2[1] )
+
+    def test_rec_increasing2(self):
+
+        recommender = FunkSVDRecs(self.save_path)
+        recs4 = recommender.recommend_items_by_ratings('5',
+                                                      [{'movie_id': AVENGERS, 'rating': 1}], num=4)
+        self.assertIsNotNone(recs4)
+        self.assertEqual(len(recs4), 4)
+
+        recs6 = recommender.recommend_items_by_ratings('5',
+                                                       [{'movie_id': AVENGERS, 'rating': 1}], num=6)
+        self.assertIsNotNone(recs6)
+        self.assertEqual(len(recs6), 6)
+
+        self.compare_recs(recs4, recs6)
+
+        recommender = FunkSVDRecs(self.save_path)
+        recs42 = recommender.recommend_items_by_ratings('5',
+                                                      [{'movie_id': AVENGERS, 'rating': 1}], num=4)
+        self.compare_recs(recs4, recs42)
+
+        recs1 = recommender.recommend_items_by_ratings('5',
+                                                      [{'movie_id': AVENGERS, 'rating': 1}], num=7)
+        recs2 = recommender.recommend_items_by_ratings('5',
+                                                      [{'movie_id': AVENGERS, 'rating': 1}], num=9)
+
+        self.compare_recs(recs1, recs2)
+
+
+    def compare_recs(self, recs1, recs2):
+        for i in range(len(recs1)):
+            self.assertEqual(recs1[i][0], recs2[i][0])
+
+
+if __name__ == '__main__':
+    unittest.main()
