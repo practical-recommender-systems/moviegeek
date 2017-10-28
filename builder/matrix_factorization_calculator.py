c08b2913e47f9925f2285fe1c7365962321c4303
Kim FalkJørgensen <kim.falk@karnovgroup.com>
2017-10-28 10:46:17 +0200

    * matrix factorization with sparse matrices.. * and some tests

--- builder/matrix_factorization_calculator.py
+++ builder/matrix_factorization_calculator.py
@@ -31,13 +31,19 @@ from moviegeeks.models import Movie
 from recommender.models import Recs
 
 
+def ensure_dir(file_path):
+    directory = os.path.dirname(file_path)
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+
 class MatrixFactorization(object):
-    Regularization = 0.015
+    Regularization = 0.02
     NumFactors = 3
     BiasLearnRate = 0.7
-    BiasReg = 0.33
+    BiasReg = 0.02
     FrequencyRegularization = 1
-    LearnRate = 0.01
+    LearnRate = 0.001
     k = 25
     all_movies_mean = 0
     number_of_ratings = 0
@@ -47,11 +53,11 @@ class MatrixFactorization(object):
     beta = 0.02
 
     iterations = 0
-    MINIMUM_IMPROVEMENT = 0.01
+    MINIMUM_IMPROVEMENT = 0.00001
 
     def __init__(self, save_path):
         self.save_path = save_path
-
+        self.saved_predictions = None
         self.user_factors = None
         self.item_factors = None
         self.user_ids = None
@@ -59,107 +65,200 @@ class MatrixFactorization(object):
 
         self.all_movies_mean = 0.0
         self.number_of_ratings = 0
-        self.MAX_ITERATIONS = 25
+        self.MAX_ITERATIONS = 20
+
+        ensure_dir(save_path)
 
     def initialize_factors(self, ratings, k=25):
-        self.k = k
         self.user_ids = set(ratings['user_id'].values)
         self.movie_ids = set(ratings['movie_id'].values)
 
-        num_users = len(self.user_ids)
-        num_items = len(self.movie_ids)
+        self.u_inx = {r: i for i, r in enumerate(self.user_ids)}
+        self.i_inx = {r: i for i, r in enumerate(self.movie_ids)}
 
-        self.all_movies_mean = self.calculate_all_movies_mean()
+        self.saved_predictions = np.zeros((len(self.user_ids), len(self.user_ids)))
+        # self.item_factors = 1 / k * np.random.randn(len(self.i_inx), k)
+        self.item_factors = np.full((len(self.i_inx), k), 0.1)
+        # self.user_factors = 1 / k * np.random.randn(len(self.u_inx), k)
+        self.user_factors = np.full((len(self.u_inx), k), 0.1)
 
-        self.item_factors = pd.DataFrame(np.random.normal(scale=1. / self.k,
-                                                          size=(num_items, self.k)),
-                                         index=self.movie_ids)
-        self.user_factors = pd.DataFrame(np.random.normal(scale=1. / self.k,
-                                                          size=(num_users, self.k)),
-                                         index=self.user_ids)
+        self.all_movies_mean = self.calculate_all_movies_mean(ratings)
+        print("user_factors are {}".format(self.user_factors.shape))
         self.user_bias = defaultdict(lambda: 0)
         self.item_bias = defaultdict(lambda: 0)
-        print('initialized factors, items: {}, users: {}'.format(num_items, num_users))
-
 
     def predict(self, user, item):
-        pq = self.item_factors.loc[item].dot(self.user_factors.loc[user])
+
+        pq = np.dot(self.item_factors[item],self.user_factors[user].T)
         b_ui = self.all_movies_mean + self.user_bias[user] + self.item_bias[item]
-        rating = b_ui + pq
-        if rating > 10:
-            rating = 10
-        elif rating < 1:
-            rating = 1
-        return rating
+        prediction = b_ui + pq
+
+        if prediction > 10:
+            prediction = 10
+        elif prediction < 1:
+            prediction = 1
+        return prediction
 
     def build(self, ratings, params):
         if params:
             k = params['k']
         self.train(ratings, k)
 
-    def train(self, ratings, k=25):
+    def loss(self, factor, i, rating, u):
 
-        self.initialize_factors(ratings, k)
-        print("training matrix factorization")
+        prediction_error = (float(rating) - self.predict(u, i))
+
+        return prediction_error
+
+    def split_data(self, min_rank, ratings):
+
+        users = self.user_ids
+
+        train_data_len = int((len(users) * 70 / 100))
+        test_users = set(random.sample(users, (len(users) - train_data_len)))
+        train_users = users - test_users
+
+        train = ratings[ratings['user_id'].isin(train_users)]
+        test_temp = ratings[ratings['user_id'].isin(test_users)].sort_values('rating_timestamp', ascending=False)
+        test = test_temp.groupby('user_id').head(min_rank)
+        additional_training_data = test_temp[~test_temp.index.isin(test.index)]
+
+        train = train.append(additional_training_data)
+
+        return test, train
+
+    def meta_parameter_train(self, ratings_df):
+
+        for k in [15, 20, 30, 40, 50, 75, 100, 125, 150, 200]:
+            self.initialize_factors(ratings_df, k)
+            self.log(str(k), "factor, iterations, train_mse, test_mse, time")
+
+            test_data, train_data = self.split_data(10,
+                                                    ratings_df)
+            columns = ['user_id', 'movie_id', 'rating']
+            ratings = train_data[columns].as_matrix()
+            test = test_data[columns].as_matrix()
+
+            self.MAX_ITERATIONS = 100
+            iterations = 0
+            index_randomized = random.sample(range(0, len(ratings)), (len(ratings) - 1))
+
+            for factor in range(k):
+                factor_iteration = 0
+                factor_time = datetime.now()
+
+                last_err = sys.maxsize
+                last_test_mse = sys.maxsize
+                finished = False
+
+                while not finished:
+                    start_time = datetime.now()
+                    train_mse = self.stocastic_gradient_descent(factor,
+                                                                index_randomized,
+                                                                ratings)
+
+                    iterations += 1
+                    test_mse = self.calculate_mse(test, factor)
+
+                    finished = self.finished(factor_iteration,
+                                             last_err,
+                                             train_mse,
+                                             last_test_mse,
+                                             test_mse)
+
+                    last_err = train_mse
+                    last_test_mse = test_mse
+                    factor_iteration += 1
+
+                    self.log(str(k), f"{factor}, {iterations}, {train_mse}, {test_mse}, {datetime.now() - factor_time}")
+
+                self.save(factor, finished)
+
+    def calculate_mse(self, ratings, factor):
+
+        def difference(row):
+            user = self.u_inx[row[0]]
+            item = self.i_inx[row[1]]
+
+            pq = np.dot(self.item_factors[item][:factor], self.user_factors[user][:factor].T)
+            b_ui = self.all_movies_mean + self.user_bias[user] + self.item_bias[item]
+            prediction = b_ui + pq
+
+            return (prediction - float(row[2])) ** 2
+
+        squared = np.apply_along_axis(difference, 1, ratings).sum()
+        return squared / ratings.shape[0]
+
+    def train(self, ratings_df, k=40):
+
+        self.initialize_factors(ratings_df, k)
+        print("training matrix factorization at {}".format(datetime.now()))
+
+        ratings = ratings_df[['user_id', 'movie_id', 'rating']].as_matrix()
+
+        index_randomized = random.sample(range(0, len(ratings)), (len(ratings) - 1))
+
+        for factor in range(k):
+            factor_time = datetime.now()
+            iterations = 0
+            last_err = sys.maxsize
+            iteration_err = sys.maxsize
+            finished = False
+
+            while not finished:
+                start_time = datetime.now()
+                iteration_err = self.stocastic_gradient_descent(factor,
+                                                              index_randomized,
+                                                              ratings)
+
+
+                iterations += 1
+                print("one epoch in {} on f={} i={} err={}".format(datetime.now() - start_time,
+                                                                   factor,
+                                                                   iterations,
+                                                                   iteration_err))
+                finished = self.finished(iterations,
+                                         last_err,
+                                         iteration_err)
+                last_err = iteration_err
+            self.save(factor, finished)
+            print("finished factor {} on f={} i={} err={}".format(factor,
+                                                                  datetime.now() - factor_time,
+                                                                  iterations,
+                                                                  iteration_err))
+
+    def stocastic_gradient_descent(self, factor, index_randomized, ratings):
 
         lr = self.LearnRate
         r = self.Regularization
+        bias_r = self.BiasReg
 
-        iterations = 0
-        last_err = 0
-        iteration_err = sys.maxsize
+        current_err = 0
+        for inx in index_randomized:
+            rating_row = ratings[inx]
 
-        while not self.finished(iterations, last_err, iteration_err):
-            start_time = datetime.now()
-            last_err = iteration_err
-            current_err = 0
-            index_randomized = random.sample(ratings.index.tolist(), len(ratings.index))
+            u = self.u_inx[rating_row[0]]
+            i = self.i_inx[rating_row[1]]
+            rating = rating_row[2]
 
-            for inx in index_randomized:
-                user_rating = ratings.ix[inx]
-                u = user_rating['user_id']
-                i = user_rating['movie_id']
-                rating = user_rating['rating']
+            err = (float(rating) - self.predict(u, i))
 
-                current_err, err = self.loss(current_err, i, rating, u)
+            self.user_bias[u] += lr * (err - bias_r * self.user_bias[u])
+            self.item_bias[i] += lr * (err - bias_r * self.item_bias[i])
 
-                self.user_bias[u] += lr * (err - r * self.user_bias[u])
-                self.item_bias[i] += lr * (err - r * self.item_bias[i])
+            user_fac = self.user_factors[u][factor].copy()
+            item_fac = self.item_factors[i][factor].copy()
 
-                user_fac = self.user_factors.loc[u].copy()
-                item_fac = self.item_factors.loc[i].copy()
-
-                self.user_factors.loc[u] += lr * (err * item_fac
+            self.user_factors[u][factor] += lr * (err * item_fac
                                                   - r * user_fac)
-                self.item_factors.loc[i] += lr * (err * user_fac
+            self.item_factors[i][factor] += lr * (err * user_fac
                                                   - r * item_fac)
+        return self.calculate_mse(ratings, factor)
 
-            iteration_err = math.sqrt(current_err / ratings.shape[0])
-            iterations += 1
+    def finished(self, iterations, last_err, current_err,
+                 last_test_mse=0, test_mse=0):
 
-            self.save()
-            print("finished iteration {} in {}, iteration error was {}".format(iterations - 1,
-                                                                               datetime.now() - start_time,
-                                                                               iteration_err))
-
-    def loss(self, current_err, i, rating, u):
-
-        r = self.Regularization
-        p = self.item_factors.loc[str(i)]
-        q = self.user_factors.loc[u]
-        p_two = p.T.dot(p).sum()
-        q_two = q.T.dot(q).sum()
-
-        prediction_error = float(rating) - self.predict(u, i)
-        regularization_error = r * (q_two + p_two)
-
-        current_err += math.pow(prediction_error + regularization_error, 2)
-
-        return current_err, prediction_error
-
-    def finished(self, iterations, last_err, current_err):
-
-        if iterations >= self.MAX_ITERATIONS or abs(last_err - current_err) < self.MINIMUM_IMPROVEMENT:
+        if last_test_mse < test_mse or iterations >= self.MAX_ITERATIONS or last_err < current_err:
             print('Finish w iterations: {}, last_err: {}, current_err {}'
                   .format(iterations, last_err, current_err))
             return True
@@ -167,31 +266,47 @@ class MatrixFactorization(object):
             self.iterations += 1
             return False
 
-    def calculate_all_movies_mean(self):
-        #todo: mean of the actual ratings, not the ones in the database
-        avg = Rating.objects.all().aggregate(Avg('rating')).values()
-        return list(avg)[0]
+    def calculate_all_movies_mean(self, ratings):
 
+        avg = ratings['rating'].sum() / ratings.shape[0]
+        return float(avg)
 
-    def save(self):
+    def save(self, factor, finished):
 
-        print("global mean: {}".format(self.all_movies_mean))
+        save_path = self.save_path + '/model/'
+        if not finished:
+            save_path += str(factor) + '/'
 
-        print("saving factors")
+        ensure_dir(save_path)
 
-        with open(self.save_path +'user_factors.json', 'w') as outfile:
-            outfile.write(self.user_factors.to_json())
-        with open(self.save_path +'item_factors.json', 'w') as outfile:
-            outfile.write(self.item_factors.to_json())
+        print("saving factors in {}".format(save_path))
+        user_bias = {uid: self.user_bias[self.u_inx[uid]] for uid in self.u_inx.keys()}
+        item_bias = {iid: self.item_bias[self.i_inx[iid]] for iid in self.i_inx.keys()}
 
-        with open(self.save_path +'user_bias.data', 'wb') as ub_file:
-            pickle.dump(dict(self.user_bias), ub_file)
+        uf = pd.DataFrame(self.user_factors,
+                          index=self.user_ids)
+        it_f = pd.DataFrame(self.item_factors,
+                            index=self.movie_ids)
 
-        with open(self.save_path +'item_bias.data', 'wb') as ub_file:
-            pickle.dump(dict(self.item_bias), ub_file)
+        with open(save_path + 'user_factors.json', 'w') as outfile:
+            outfile.write(uf.to_json())
+        with open(save_path + 'item_factors.json', 'w') as outfile:
+            outfile.write(it_f.to_json())
+        with open(save_path + 'user_bias.data', 'wb') as ub_file:
+            pickle.dump(user_bias, ub_file)
+        with open(save_path + 'item_bias.data', 'wb') as ub_file:
+            pickle.dump(item_bias, ub_file)
+
+    def log(self, filename, logtext):
+        path = self.save_path + '/meta/' + filename + '.csv'
+        ensure_dir(path)
+
+        with open(path, 'a') as log_file:
+            log_file.write(logtext + '\n')
+
 
 def load_all_ratings():
-    columns = ['user_id', 'movie_id', 'rating', 'type']
+    columns = ['user_id', 'movie_id', 'rating', 'type', 'rating_timestamp']
 
     ratings_data = Rating.objects.all().values(*columns)
 
@@ -199,8 +314,10 @@ def load_all_ratings():
     ratings['rating'] = ratings['rating'].astype(float)
     return ratings
 
+
 if __name__ == '__main__':
     print("Calculating matrix factorization...")
 
-    MF = MatrixFactorization(save_path='./models/funk_svd/')
-    MF.train(load_all_ratings())
+    MF = MatrixFactorization(save_path='./models/prod/')
+    # MF.meta_parameter_train(load_all_ratings())
+    MF.train(load_all_ratings(), k=40)
