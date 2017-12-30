9f3e1cc572511ba7fe2199bb903a61724fffcc45
Kim FalkJørgensen <kim.falk@karnovgroup.com>
2017-12-30 18:14:47 +0100

    * updated parameter name

--- recommender/urls.py
+++ recommender/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     url(r'^ar/(?P<user_id>\w+)/$',
         views.recs_using_association_rules,
         name='recs_using_association_rules'),
-    url(r'^sim/user/(?P<user_id>\w+)/(?P<type>\w+)/$',
+    url(r'^sim/user/(?P<user_id>\w+)/(?P<sim_method>\w+)/$',
         views.similar_users, name='similar_users'),
     url(r'^cb/item/(?P<content_id>\w+)/$',
         views.similar_content, name='similar_content'),
@@ -19,6 +19,10 @@ urlpatterns = [
         views.recs_cf, name='recs_cb'),
     url(r'^funk/user/(?P<user_id>\w+)/$',
         views.recs_funksvd, name='recs_funksvd'),
+    url(r'^fwls/user/(?P<user_id>\w+)/$',
+        views.recs_fwls, name='recs_fwls'),
+    url(r'^bpr/user/(?P<user_id>\w+)/$',
+        views.recs_bpr, name='recs_fwls'),
     url(r'^pop/user/(?P<user_id>\w+)/$',
         views.recs_pop, name='recs_pop')
 ]
