197428e1e64a82d037472eab478bfbd520486d86
Kim FalkJørgensen <kim.falk@karnovgroup.com>
2017-12-30 18:14:14 +0100

    * updated genres links, so hyphens doesnt create problems

--- moviegeeks/urls.py
+++ moviegeeks/urls.py
@@ -5,6 +5,6 @@ from moviegeeks import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^movie/(?P<movie_id>\d+)/$', views.detail, name='detail'),
-    url(r'^genre/(?P<genre_id>\w+)/$', views.genre, name='genre'),
+    url(r'^genre/(?P<genre_id>[\w-]+)/$', views.genre, name='genre'),
     url(r'^search/$', views.search_for_movie, name='search_for_movie'),
 ]
