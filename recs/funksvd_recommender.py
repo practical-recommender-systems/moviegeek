21a8cecf01b9b0853645eccbb7fb34cc94062fbd
Kim FalkJørgensen <kim.falk@karnovgroup.com>
2018-01-02 07:40:16 +0100

    * Automatic evaluation

--- recs/funksvd_recommender.py
+++ recs/funksvd_recommender.py
@@ -7,6 +7,7 @@ from recommender.models import Recs
 from analytics.models import Rating
 from recs.base_recommender import base_recommender
 import collections
+import decimal
 import numpy as np
 import pandas as pd
 import pickle
@@ -14,10 +15,10 @@ import pickle
 
 class FunkSVDRecs(base_recommender):
 
-    def __init__(self, save_path='/Users/u0157492/Projects/moviegeek/builder/models/funkSVD/2017-11-14 22:08:47.116843//model/100/'):
+    def __init__(self, save_path='./models/funkSVD/2018-01-01 10:01:34.179393/model/'):
         self.save_path = save_path
-        self.load_model(save_path)
-        self.avg = list(Rating.objects.all().aggregate(Avg('rating')).values())[0]
+        self.model_loaded = False
+        self.avg = Decimal(list(Rating.objects.all().aggregate(Avg('rating')).values())[0])
 
     def load_model(self, save_path):
         with open(save_path + 'user_bias.data', 'rb') as ub_file:
@@ -29,6 +30,7 @@ class FunkSVDRecs(base_recommender):
         with open(save_path + 'item_factors.json', 'r') as infile:
             self.item_factors = pd.DataFrame(json.load(infile)).T
         self.ordered_item_bias = list(collections.OrderedDict(sorted(self.item_bias.items())).values())
+        self.model_loaded = True
 
     def set_save_path(self, save_path):
         self.save_path = save_path
@@ -36,6 +38,10 @@ class FunkSVDRecs(base_recommender):
         self.load_model(save_path)
 
     def predict_score(self, user_id, item_id):
+
+        if not self.model_loaded:
+            self.load_model(self.save_path)
+
         rec = Recs.objects.filter(user_id = user_id, item_id=item_id).first()
         if rec is None:
             return 0
@@ -43,11 +49,18 @@ class FunkSVDRecs(base_recommender):
             return rec.rating
 
     def recommend_items(self, user_id, num=6):
+
+        if not self.model_loaded:
+            self.load_model(self.save_path)
+
         active_user_items = Rating.objects.filter(user_id=user_id).order_by('-rating')[:100]
 
         return self.recommend_items_by_ratings(user_id, active_user_items.values())
 
     def recommend_items_by_ratings(self, user_id, active_user_items, num=6):
+
+        if not self.model_loaded:
+            self.load_model(self.save_path)
 
         rated_movies = {movie['movie_id']: movie['rating'] for movie in active_user_items}
         recs = {}
@@ -58,16 +71,19 @@ class FunkSVDRecs(base_recommender):
             scores = self.item_factors.T.dot(user)
 
             sorted_scores = scores.sort_values(ascending=False)
-            result = sorted_scores[:num + len(rated_movies)]
+            result = sorted_scores[:num + len(rated_movies)].astype(decimal.Decimal)
             user_bias = 0
 
             if user_id in self.user_bias.keys():
                 user_bias = self.user_bias[user_id]
             elif int(user_id) in self.user_bias.keys():
                 user_bias = self.user_bias[int(user_id)]
-            result += user_bias + self.avg
+                print(f'it was an int {user_bias}')
 
-            recs = {r[0]: {'prediction': r[1] + self.item_bias[r[0]]}
+            rating = float(user_bias + self.avg)
+            result += rating
+
+            recs = {r[0]: {'prediction': r[1] + float(self.item_bias[r[0]])}
                     for r in zip(result.index, result) if r[0] not in rated_movies}
 
         sorted_items = sorted(recs.items(), key=lambda item: -float(item[1]['prediction']))[:num]
