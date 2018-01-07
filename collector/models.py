848c9833b9e02b3322c1c438b9e53132a24d9704
Kim FalkJørgensen <kim.falk@karnovgroup.com>
2018-01-07 18:31:51 +0100

    * house keeping

--- collector/models.py
+++ collector/models.py
@@ -2,11 +2,13 @@ from django.db import models
 
 
 class Log(models.Model):
-    created = models.DateTimeField('date happened') #A
+    created = models.DateTimeField('date happened')
     user_id = models.CharField(max_length=16)
     content_id = models.CharField(max_length=8)
     event = models.CharField(max_length=200)
     session_id = models.CharField(max_length=128)
 
     def __str__(self):
-        return "user_id: {}, content_id: {}, event: {}".format(self.user_id, str(self.content_id), self.event) #B
+        return "user_id: {}, content_id: {}, event: {}".format(self.user_id,
+                                                               str(self.content_id),
+                                                               self.event)
