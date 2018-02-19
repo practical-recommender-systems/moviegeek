d86a07ee20d94ef0a38b737323c107641daa0652
Kim FalkJørgensen <kim.falk@karnovgroup.com>
2018-02-19 23:58:39 +0100

    * Change config to use sqlite3, and travis script will migrate django database

--- prs_project/settings.py
+++ prs_project/settings.py
@@ -69,22 +69,22 @@ WSGI_APPLICATION = 'prs_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#    }
-#}
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'moviegeek',
-        'USER': 'postgres',
-        'PASSWORD': 'hullo1!',
-        'HOST': 'localhost',
-        'PORT': 5432
-    }
+   'default': {
+       'ENGINE': 'django.db.backends.sqlite3',
+       'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+   }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'moviegeek',
+#         'USER': 'postgres',
+#         'PASSWORD': 'hullo1!',
+#         'HOST': 'localhost',
+#         'PORT': 5432
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
