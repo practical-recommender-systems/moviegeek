9fe67b0fb7783c719d8c7cc0292568e2b906dd03
Kim Falk <kfalk@liveintent.com>
2018-11-20 14:30:55 +0100

    Updates the charField size for content ids

--- analytics/models.py
+++ analytics/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Rating(models.Model):
     user_id = models.CharField(max_length=16)
-    movie_id = models.CharField(max_length=8)
+    movie_id = models.CharField(max_length=16)
     rating = models.DecimalField(decimal_places=2, max_digits=4)
     rating_timestamp = models.DateTimeField()
     type = models.CharField(max_length=8, default='explicit')
